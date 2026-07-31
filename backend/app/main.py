b4ad31245132481d80ea58ba5d3cac811191467b
Srishti <sonikasharma35133@gmail.com>
2026-07-31 13:37:52 +0530

    Fix main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,35 +1,24 @@
-#source venv/Scripts/activate
-#uvicorn app.main:app --reload
-from fastapi import  FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from app.routers import auth_router, sms_router, transactions, budgets,analytics
-
-
-
+from app.routers import auth_router, sms_router, transactions, budgets, analytics
 
 app = FastAPI()
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
-    allow_credentials=True,
-    allow_methods=["*"],
-     allow_headers=["*"],
- )
 
 origins = [
     "http://localhost:5173",
     "https://upi-expense-tracker-zeta.vercel.app",
 ]
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(sms_router.router)
 app.include_router(auth_router.router)
 app.include_router(transactions.router)
 app.include_router(budgets.router)
 app.include_router(analytics.router)
-
-
